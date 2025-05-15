c61634718749a6cc85d18c239f41267a2819fb1f
diiidar <151949765+diiidar@users.noreply.github.com>
2025-05-15 18:18:31 +0500

    switching to easyocr

--- app.py
+++ app.py
@@ -2,16 +2,21 @@
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 from flask import Flask, request, jsonify, render_template
-import pytesseract
+import easyocr
 from utils import preprocessing
-import cv2 as cv
 import math
 from PIL import Image
 from transformers import BartForConditionalGeneration, BartTokenizer
+import numpy as np
 
 app = Flask(__name__)
-model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn").to('cpu')
+
+device = "cpu"
+model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn").to(device)
 tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
+
+# Инициализация EasyOCR reader один раз, с нужными языками, например, английский
+reader = easyocr.Reader(['en'])
 
 @app.route("/")
 def index():
@@ -23,14 +28,15 @@ def process_image():
     remove_grid_line = request.form.get("removeGridLine", "false") == "true"
 
     image = Image.open(image_file).convert("RGB")
+    image = np.array(image)
     
     if remove_grid_line:
         image = preprocessing.preprocess(image_file)
 
-
-    # OCR
-    extracted_text = pytesseract.image_to_string(image)
-    print('extracted text:',extracted_text) 
+    # OCR с easyocr
+    results = reader.readtext(image)
+    extracted_text = " ".join([res[1] for res in results])  # объединяем все распознанные тексты
+    print('extracted text:', extracted_text)
 
     # Summarization
     summary = summarize(extracted_text)
@@ -38,12 +44,17 @@ def process_image():
     return jsonify({"summary": summary})
 
 def summarize(text):
-    global device, model, tokenizer
     word_count = len(text.split())
     min_length = int(10 * math.sqrt(word_count + 64) - 80)
 
-    inputs = tokenizer(text, return_tensors="pt", max_length=1024, truncation=True)
-    summary_ids = model.generate(inputs["input_ids"], max_length=700, min_length=min_length, length_penalty=1.7, num_beams=4)
+    inputs = tokenizer(text, return_tensors="pt", max_length=1024, truncation=True).to(device)
+    summary_ids = model.generate(
+        inputs["input_ids"],
+        max_length=700,
+        min_length=min_length,
+        length_penalty=1.7,
+        num_beams=4,
+    )
 
     return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
